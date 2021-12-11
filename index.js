4825a086d2541e016e555933b535e83bb81617cd
sieva-cmd <musyokasieva@gmail.com>
2021-12-11 22:10:15 +0300

    add javascript functions to generate the akan name

---
+++ index.js
@@ -0,0 +1,89 @@
+var myFunction = function () {
+
+    var inputDay = document.getElementById('exampleInputDay').value;
+    var inputMonth = document.getElementById('exampleInputMonth').value;
+    var inputYear = document.getElementById('exampleInputYear').value;
+    var gender = document.querySelector('input[ name = "Gender"]:checked').value;
+
+    if (inputDay <= 0 || inputDay > 31) {
+        alert("invalid date");
+
+    } else if (inputMonth <= 0 || inputMonth > 12) {
+        alert("invalid month!!");
+    } else if (!inputYear.match(/^\d\d\d\d$/)) {
+        alert("invalid year!");
+    } else {
+
+
+        var calculation = function (inputDay, inputMonth, inputYear) {
+            var inputDay = document.getElementById('exampleInputDay').value;
+            var inputMonth = document.getElementById('exampleInputMonth').value;
+            var inputYear = document.getElementById('exampleInputYear').value;
+
+            var dd = parseInt(inputDay);
+            var mm = parseInt(inputMonth);
+            var cc = parseInt(inputYear.slice(0, 2));
+            var yy = parseInt(inputYear);
+
+
+            var result = (cc / 4 - 2 * cc - 1 + (5 * yy) / 4 + (26 * (mm + 1)) / 10 + dd) % 7;
+            var day = Math.ceil(result);
+
+            var gender = document.querySelector('input[name ="Gender"]:checked').value;
+            if (gender === "female") {
+                var name
+                switch (day) {
+                    case 0:
+                        name = "Akosua";
+                        break;
+                    case 1:
+                        name = "Adwoa";
+                        break;
+                    case 2:
+                        name = "Abenaa";
+                        break;
+                    case 3:
+                        name = "Akua";
+                        break;
+                    case 4:
+                        name = "Yaa";
+                        break;
+                    case 5:
+                        name = "Afua";
+                        break;
+                    case 6:
+                        name = "Ama";
+                        break;
+
+                }
+
+            }
+            else {
+                switch (day) {
+                    case 0:
+                        name = "mutua";
+                        break;
+                    case 1:
+                        name = "musyoka";
+                        break;
+                    case 2: name = "muithya";
+                        break;
+                    case 3: name = "mutuku";
+                        break;
+                    case 4: name = "mwendwa";
+                        break;
+                    case 5: name = "peter";
+                        break;
+                    case 6: name = "juma";
+                        break;
+
+                }
+            }document.getElementById("error").innerHTML = ("Your Akan name is " +  name);
+
+
+
+        }
+        calculation();
+    }
+
+}
